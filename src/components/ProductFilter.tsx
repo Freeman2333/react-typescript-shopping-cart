a29c9967be07c37a5054b6813b53b4b8a48e1a54
Sergii Popov <freeman23@bigmir.net>
2021-07-19 17:22:38 +0300

    new

---
+++ src/components/ProductFilter.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./../config/styles/product-filter.scss";
+import { connect } from "react-redux";
+import { Product } from "./../store/model/product";
+import ProductService from "./../store/services/products.service";
+const ProductFilter = () => {
+  return <div>ProductFilter</div>;
+};
+
+export default ProductFilter;
--- src/screens/Products.tsx
+++ src/screens/Products.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import ProductFilter from "../components/ProductFilter";
 import ProductsList from "../components/ProductsList";
 
 const Products = () => {
   return (
     <div className="row mt-5">
-      <div className="col-md-3 order-md-1 mb-4">ProductFilter</div>
+      <div className="col-md-3 order-md-1 mb-4">
+        <ProductFilter />
+      </div>
       <div className="col-md-9 order-md-2">
         <ProductsList />
       </div>
